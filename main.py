425d427b8aa4ff7eff985342344f3503b84d00f2
sophiiecheng <75765535+sophiiecheng@users.noreply.github.com>
2023-06-02 12:06:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,70 @@
-print("Hello World")
-print("Goodbye World")
-print("I'm back again")
-print("I'm leaving agin!")
+import json
+
+# Initialize the game board
+board = [[' ' for _ in range(3)] for _ in range(3)]
+current_player = 'X'
+
+# Function to print the game board
+def print_board(board):
+    for row in board:
+        print('|'.join(row))
+        print('-' * 5)
+
+# Function to make a move on the board
+def make_move(row, col):
+    if board[row][col] == ' ':
+        board[row][col] = current_player
+    else:
+        print("Invalid move. Try again.")
+
+# Function to switch the current player
+def switch_player():
+    global current_player
+    if current_player == 'X':
+        current_player = 'O'
+    else:
+        current_player = 'X'
+
+# Function to save the game state to a JSON file
+def save_game():
+    game_state = {
+        'board': board,
+        'current_player': current_player
+    }
+    with open('tic_tac_toe_game.json', 'w') as file:
+        json.dump(game_state, file)
+    print("Game saved successfully!")
+
+# Function to load the game state from a JSON file
+def load_game():
+    global board, current_player
+    try:
+        with open('tic_tac_toe_game.json', 'r') as file:
+            game_state = json.load(file)
+            board = game_state['board']
+            current_player = game_state['current_player']
+        print("Game loaded successfully!")
+    except FileNotFoundError:
+        print("No saved game found.")
+
+# Main game loop
+while True:
+    print_board(board)
+
+    # Player's move
+    row = int(input("Enter the row (0-2): "))
+    col = int(input("Enter the column (0-2): "))
+    make_move(row, col)
+
+    # Check for a winner
+    # ...
+
+    # Switch to the next player
+    switch_player()
+
+    # Save or load the game
+    option = input("Enter 's' to save the game, 'l' to load a saved game, or any other key to continue: ")
+    if option == 's':
+        save_game()
+    elif option == 'l':
+        load_game()
